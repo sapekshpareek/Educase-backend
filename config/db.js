3339ca14af5b519b3ff1a5ed7ed37c54f47ae2a4
sapekshpareek <sapekshpareek136@gmail.com>
2025-06-14 21:07:52 +0530

    Update in DB

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '1111421',
     database: process.env.DB_NAME || 'school_management_api'
